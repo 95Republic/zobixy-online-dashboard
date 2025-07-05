40f20bdf9051a3b603efff237cfd40043b53eaae
TheGroveDarkKitchen <143114351+TheGroveDarkKitchen@users.noreply.github.com>
2025-07-05 12:05:52 +0200

    Updating api.js to cater for production backend api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+const local = 'http://localhost:5000'
+const production = 'https://zobixy-online-backend-api.onrender.com'
 
+let api_url = ''
+let mode = 'prod'
+
+if (mode === 'prod') {
+    api_url = production
+} else {
+    api_url = local
+}
 const api = axios.create({
-    baseURL : 'http://localhost:5000/api'
+    baseURL : `${api_url}/api`,
+    withCredentials: true
 })
 
 export default api
