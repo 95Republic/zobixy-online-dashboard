9945e5a48dbb75079ead58b468174709ea2f0406
TheGroveDarkKitchen <143114351+TheGroveDarkKitchen@users.noreply.github.com>
2025-07-05 15:33:33 +0200

    update 5

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,22 @@
-    import io from 'socket.io-client'
-    
-    //loader custom css 
-    export const overrideStyle = {
-        display : 'flex',
-        margin : '0 auto',
-        height : '24px',
-        justifyContent : 'center',
-        alignItems : 'center'
-    }
+// utils/socket.js or utils.js
 
-    export const socket = io('http://localhost:5000/')
+import { io } from 'socket.io-client';
+
+// Use environment variable for Socket.IO URL
+const SOCKET_URL = process.env.mode === 'prod' ? [process.env.REACT_APP_PROD_SOCKET_UR] : 
+        [process.env.REACT_APP_LOCAL_SOCKET_URL];
+        
+// Export socket instance with options
+export const socket = io(SOCKET_URL, {
+  withCredentials: true,
+  transports: ['websocket'], // prefer websockets over polling
+});
+
+// Custom loader style used in components
+export const overrideStyle = {
+  display: 'flex',
+  margin: '0 auto',
+  height: '24px',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
