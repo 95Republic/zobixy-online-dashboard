ecddd4561d603b2d2e28d0308297805a4fc59ba7
TheGroveDarkKitchen <143114351+TheGroveDarkKitchen@users.noreply.github.com>
2025-07-08 13:00:31 +0200

    status 7

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,22 +1,17 @@
-// utils/socket.js or utils.js
-
 import { io } from 'socket.io-client';
 
 // Use environment variable for Socket.IO URL
 const SOCKET_URL = process.env.REACT_APP_MODE === 'prod' ? process.env.REACT_APP_PROD_SOCKET_URL : 
         process.env.REACT_APP_LOCAL_SOCKET_URL;
 
-// Export socket instance with options
-export const socket = io(SOCKET_URL, {
-  withCredentials: true,
-  transports: ['websocket'], // prefer websockets over polling
-});
-
 // Custom loader style used in components
+console.log("SOCKET URL: " + SOCKET_URL)
+console.log("MODE: " + process.env.REACT_APP_MODE)
 export const overrideStyle = {
   display: 'flex',
   margin: '0 auto',
   height: '24px',
   justifyContent: 'center',
   alignItems: 'center',
-};
+}
+export const socket = io(SOCKET_URL)
